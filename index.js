fdf2ba725d43148eb6b5e9afe5f5a6ac81a50125
unknown <c.vishnu.vasudevan2@gmail.com>
2024-07-10 15:56:29 +0530

    changes

--- index.js
+++ index.js
@@ -6,14 +6,16 @@ import mongoose from 'mongoose'
 import cors from 'cors'
 import { taskRouter } from './routes/taskRoutes.js'
 const app = express()
+const {MONGO_PASSWORD} = process.env
 
+const connectionString = `mongodb+srv://admin:${MONGO_PASSWORD}@cluster0.rhqvh03.mongodb.net/ToDoList?retryWrites=true&w=majority`
 
 app.use(cors())
 app.use(express.json())
 app.use('/tasks', taskRouter)
-const {MONGO_URL} = process.env
 
-mongoose.connect(MONGO_URL)
+
+mongoose.connect(connectionString)
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
